48f833ecf6e4e4da7b36c7f9bb9c060444b5a2f7
Zocker7sea <48686970+Zocker7sea@users.noreply.github.com>
2022-12-13 20:28:17 +0100

    test

--- app/src/main/java/com/example/testbutton/Bestaetigungsmeldung.java
+++ app/src/main/java/com/example/testbutton/Bestaetigungsmeldung.java
@@ -10,5 +10,7 @@ public class Bestaetigungsmeldung extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bestaetigungsmeldung);
+
+        //val String
     }
 }
